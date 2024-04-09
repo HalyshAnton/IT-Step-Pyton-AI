32ecbea019dd7765af333e442efc5ceab505bcb9
HalyshAnton <kyhar007@gmail.com>
2024-04-09 20:41:53 +0300

    add more logic

--- practice4.py
+++ practice4.py
@@ -57,29 +57,81 @@ class BoardingQueue:
         while node is not None and node.data != data:
             node = node.next
 
+        # Якщо потрібного вузла немає
         if node is None:
             print('Немає пасажира в черзі')
             return
 
-        # Переміщення вперед
+        # Якщо пасажир в кінці черги
+        if node.next is None:
+            print('Пасажир в кінці черги')
+            return
 
-        red = node.prev
-        green = node
-        blue = node.next
-        black = node.next.next # blue.next
+        # Якщо node не в голові, а node.next не в хвості
+        if node.prev and node.next.next:
+            red = node.prev
+            green = node
+            blue = node.next
+            black = node.next.next # blue.next
+            # змінюємо посилання вперед
+            red.next, green.next, blue.next = blue, black, green
+            # змінюємо посилання назад
+            green.prev, blue.prev, black.prev = blue, red, green
 
-        # змінюємо посилання вперед
-        red.next, green.next, blue.next = blue, black, green
+        # Якщо node в голові, а node.next не в хвості
+        elif node.next.next:
+            red = node.prev # None, немає next
+            green = node
+            blue = node.next
+            black = node.next.next
 
-        # змінюємо посилання назад
-        green.prev, blue.prev, black.prev = blue, red, green
+            # змінюємо посилання вперед
+            green.next, blue.next = black, green # без red.next
+            # змінюємо посилання назад
+            green.prev, blue.prev, black.prev = blue, red, green
+
+            # тепер blue в голові
+            self.head = blue
+
+        # Якщо node не в голові, а node.next в хвості
+        elif node.prev:
+            red = node.prev
+            green = node
+            blue = node.next
+            black = node.next.next # None, немає prev
+
+            # змінюємо посилання вперед
+            red.next, green.next, blue.next = blue, black, green
+            # змінюємо посилання назад
+            green.prev, blue.prev = blue, red # без black.prev
+
+            # тепер green в хвості
+            self.tail = green
+
+        # Якщо node голові і node.next в хвості
+        else:
+            red = node.prev # None, немає next
+            green = node
+            blue = node.next
+            black = node.next.next # None, немає prev
+
+            # змінюємо посилання вперед
+            green.next, blue.next = black, green # без red.next
+            # змінюємо посилання назад
+            green.prev, blue.prev = blue, red, green # без black.prev
+
+            # тепер blue в голові, а green в хвості
+            self.head = blue
+            self.tail = green
 
     def print(self):
         node = self.head
 
         while node is not None:
-            print(node.data, end='->')
+            print(node.data, end=' -> ')
             node = node.next
+
+        print()
 
 
 my_list = BoardingQueue()
@@ -92,11 +144,23 @@ my_list.append(5)
 my_list.print()
 print()
 
+print('3 вперед')
 my_list.move_forward(3)
 my_list.print()
 print()
 
+print('5 вперед')
 my_list.move_forward(5)
+my_list.print()
+print()
+
+print('3 вперед')
+my_list.move_forward(3)
+my_list.print()
+print()
+
+print('1 вперед')
+my_list.move_forward(1)
 my_list.print()
 print()
 
