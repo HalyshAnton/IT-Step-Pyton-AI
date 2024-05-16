9dd900d8a8bb6304cc246465b1f0173b6a02b05d
HalyshAnton <kyhar007@gmail.com>
2024-05-16 19:24:35 +0300

    add lesson4

--- 1.sql
+++ 1.sql
@@ -1,24 +1,29 @@
--- ВИВЕСТИ ПРОФЕСІЮ, З НАЙМЕНШОЮ КІЛЬКІСТЮ СТУДЕНТІВ
+-- CREATE TABLE IF NOT EXISTS FAMILYINFO(
+-- 	ID SERIAL PRIMARY KEY,
+-- 	INFO VARCHAR(30),
+-- 	STUDENT_ID INT REFERENCES STUDENT(ID)
+-- );
 
--- СОРТУВАННЯ( ПОВІЛЬНО)
-SELECT JOB, COUNT(*) AS "JOB COUNT"
-FROM STUDENT
-GROUP BY JOB
-ORDER BY "JOB COUNT" DESC
-LIMIT 1;
+-- INSERT INTO FAMILYINFO(
+-- 	INFO,
+-- 	STUDENT_ID
+-- ) VALUES
+-- ('BROTHER', 3),
+-- ('SISTER', 11),
+-- ('SISTER', 1),
+-- ('MOTHER', 1),
+-- ('FATHER', 1);
+
+---ВИВЕСТИ ДАНІ ПРО СТУДЕНТІВ РАЗОМ З ДАНИМИ ПРО СІМ'Ю
+SELECT *
+FROM FAMILYINFO JOIN STUDENT ON FAMILYINFO.STUDENT_ID = STUDENT.ID
+
+-- ВИВЕСТИ ДАНІ СТУДЕНТІВ, ПРО ЯКИХ Є ДАНІ ПРО САМІ
+-- SELECT *
+-- FROM STUDENT
+-- WHERE ID IN (SELECT STUDENT_ID
+-- 			 FROM FAMILYINFO
+-- )
 
 
---- ВИКОРИСТАННЯ ТИМЧАСОВОЇ ТАБЛИЦІ( ШВИДШЕ)
-WITH COUNTS_TABLE AS (
-	SELECT JOB, COUNT(*) AS "JOB COUNT"
-	FROM STUDENT
-	GROUP BY JOB
-)
-
-SELECT JOB
-FROM COUNTS_TABLE
-WHERE "JOB COUNT" = (
-	SELECT MAX("JOB COUNT")
-	FROM COUNTS_TABLE
-)
 
