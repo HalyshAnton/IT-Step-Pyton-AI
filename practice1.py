939fe17cc30c48921f26eab88bfbfc33663642e4
HalyshAnton <kyhar007@gmail.com>
2024-05-23 21:47:52 +0300

    add lesson2

--- practice1.py
+++ practice1.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, func, and_, or_
+from sqlalchemy import create_engine, MetaData, func, and_, or_, extract
 from sqlalchemy.orm import sessionmaker
 import json
 
@@ -21,8 +21,14 @@ session = Session()
 docs = metadata.tables['doctors']
 specs = metadata.tables['specializations']
 docsspecs = metadata.tables['doctorsspecializations']
+doctors_vacations_table = metadata.tables['vacations']
+Departments = metadata.tables['departments']
+Sponsors = metadata.tables['sponsors']
+Donations = metadata.tables['donations']
+Wards = metadata.tables['wards']
 
 
+# звіт 1
 def report_doctor_specializations():
     result = session.query(docs.c.name.label('doctorname'),
                            docs.c.surname,
@@ -33,25 +39,78 @@ def report_doctor_specializations():
     if result:
         for row in result:
             print(f"{row.doctorname} {row.surname} with specialization {row.specialization_name}")
+    else:
+        print('Пусто')
 
     # and - &
     # or - |
+    # not - ~
     # (name == 'hjk') & (salary > 20)
 
+#звіт 2
 def report_doctors_salary_not_on_vacation():
-    pass
+    query = session.query(docs.c.surname, docs.c.salary) \
+                   .outerjoin(doctors_vacations_table, (docs.c.id == doctors_vacations_table.c.doctor_id) &
+                              ((doctors_vacations_table.c.start_date <= func.current_date()) & (func.current_date() <= doctors_vacations_table.c.end_date))) \
+                   .filter(doctors_vacations_table.c.id == None)  # Фільтруємо тих, хто не в відпустці
+    result = query.all()
+    for row in result:
+        print(f"{row.surname} - {row.salary}")
 
-
+#звіт 3
 def report_wards_depatment():
-    pass
+    department_name = input("Введіть назву відділення: ")
 
+    query = session.query(Wards.c.name) \
+        .join(Departments, Wards.c.department_id == Departments.c.id) \
+        .filter(Departments.c.name == department_name)
 
+    result = query.all()
+
+    if result:
+        print(f"Назви палат у відділенні '{department_name}':")
+        for row in result:
+            print(row.name)
+    else:
+        print(f"Відділення '{department_name}' або палати у цьому відділенні не знайдено.")
+
+#звіт 4
 def repor_donation_of_last_mounth():
-    pass
+    # Введіть номер місяця (1-12)
+    month = int(input("Введіть номер місяця: "))
 
+    query = session.query(Departments.c.name.label('Department'), Sponsors.c.name.label('Sponsor'),
+                          Donations.c.amount.label('Amount'), Donations.c.date.label('DonationDate')) \
+                   .join(Donations, Departments.c.id == Donations.c.department_id) \
+                   .join(Sponsors, Sponsors.c.id == Donations.c.sponsor_id) \
+                   .filter(extract('month', Donations.c.date) == month)
 
+    result = query.all()
+
+    if result:
+        print(f"Усі пожертвування за {month}-й місяць:")
+        for row in result:
+            print(f"Відділення: {row.Department}, Спонсор: {row.Sponsor}, Сума: {row.Amount}, Дата: {row.DonationDate}")
+    else:
+        print(f"Пожертвувань за {month}-й місяць не знайдено.")
+#звіт 5
 def report_departaments_donation():
-    pass
+    sponsor_name = input("Введіть назву спонсора: ")
+
+    query = session.query(Departments.c.name.label('Department')) \
+                   .distinct() \
+                   .join(Donations, Departments.c.id == Donations.c.department_id) \
+                   .join(Sponsors, Sponsors.c.id == Donations.c.sponsor_id) \
+                   .filter(Sponsors.c.name == sponsor_name)
+
+    result = query.all()
+
+    if result:
+        print(f"Назви відділень, які спонсоруються компанією '{sponsor_name}':")
+        for row in result:
+            print(row.Department)
+    else:
+        print(f"Відділень, які спонсоруються компанією '{sponsor_name}' не знайдено.")
 
 
 while True:
